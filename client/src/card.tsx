89816e6e155314e1d1de0fb5cb4a5558cdfeb664
jenssandgren <jenssandgren@gmail.com>
2022-04-06 15:38:51 +0200

    updating

--- client/src/card.tsx
+++ client/src/card.tsx
@@ -7,12 +7,8 @@ export default function Card() {
       <div className="relative h-2/5">
 
         <span className="font-bold text-lg m-2">PU-Movie: The King's Man</span><br/>
-        <span className="m-4">40 kr</span> <br/>
-        <span className="eventPrice">A dope spy-movie about agents in the UK</span> <br/>
-        <a href="https://www.facebook.com/events/340855818101825/?ref=newsfeed">
-          <span className="m-4 text-blue-800 hover:underline">Länk</span>
-        </a>
-        
+        <span className="m-4">40 kr</span>
+        <span className="eventPrice">A dope spy-movie about agents in the UK</span>
         <div className="absolute bottom-1 right-2">
           <span className="">PU</span>
           <div className="bg-[url('./img/pu.jpg')] w-8 h-8 rounded-full inline-block bg-cover"></div>
