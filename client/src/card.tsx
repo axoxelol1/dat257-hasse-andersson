811301ae2f94c5eb82152986a2fb5186c55d2a94
jenssandgren <jenssandgren@gmail.com>
2022-04-06 15:17:50 +0200

    hej

--- client/src/card.tsx
+++ client/src/card.tsx
@@ -5,6 +5,7 @@ export default function Card() {
       <div className="relative h-2/5">
         <span className="font-bold text-lg">Bubbelprovning</span><br/>
         <span className="eventPrice">250 kr</span>
+        <span className="eventPrice">A dope spy-movie about agents in the UK</span>
         <div className="absolute bottom-1 right-2 border-2 border-red-500">
           <span className="hostName">PU</span>
           <div className="bg-[url('./img/pu.jpg')] w-8 h-8 inline-block bg-cover"></div>
