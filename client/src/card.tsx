0bbd2e47b528b4842bfefb8f2a3ead00b916f141
Jacob <jacob.bengtsson@rocketmail.com>
2022-04-06 14:45:12 +0200

    Add rudimentary but finished card

--- client/src/card.tsx
+++ client/src/card.tsx
@@ -1,13 +1,13 @@
 export default function Card() {
   return (
-    <div className="border-2 border-black w-72 h-96">
-      <div className="image"></div>
-      <div className="textContainer">
-        <span className="eventName">Bubbelprovning</span>
+    <div className="border-2 border-black w-72 h-96 flex-col">
+      <div className="bg-[url('./img/kingsman.jpg')] h-3/5"></div>
+      <div className="relative h-2/5">
+        <span className="font-bold text-lg">Bubbelprovning</span><br/>
         <span className="eventPrice">250 kr</span>
-        <div className="host">
+        <div className="absolute bottom-1 right-2 border-2 border-red-500">
           <span className="hostName">PU</span>
-          <div className="hostImage"></div>
+          <div className="bg-[url('./img/pu.jpg')] w-8 h-8 inline-block bg-cover"></div>
         </div>
       </div>
     </div>
