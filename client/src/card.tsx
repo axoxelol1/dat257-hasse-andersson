f70180809f9079c363e359763748e7f44f29c573
Jacob <jacob.bengtsson@rocketmail.com>
2022-04-06 15:24:22 +0200

    Add link

--- client/src/card.tsx
+++ client/src/card.tsx
@@ -7,7 +7,10 @@ export default function Card() {
       <div className="relative h-2/5">
 
         <span className="font-bold text-lg m-2">PU-Movie: The King's Man</span><br/>
-        <span className="m-4">40 kr</span>
+        <span className="m-4">40 kr</span><br/>
+        <a href="https://www.facebook.com/events/340855818101825/?ref=newsfeed">
+          <span className="m-4 text-blue-800 hover:underline">Länk</span>
+        </a>
 
         <div className="absolute bottom-1 right-2">
           <span className="">PU</span>
