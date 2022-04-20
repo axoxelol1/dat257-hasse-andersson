12c08a7315ec478668e8273efc4aaaab0866f0b3
jenssandgren <jenssandgren@gmail.com>
2022-04-20 11:01:36 +0200

    added comments to Event interface

--- client/lib/types.ts
+++ client/lib/types.ts
@@ -1,3 +1,6 @@
+// Interface that defines the core attrtibutes of events that are to be displayed on the front end.
+// Includes both mandatory and optional attributes, with "eventImageUrl" being optional and the other ones mandatory
+
 export interface Event {
     id: string;
     title: string;
