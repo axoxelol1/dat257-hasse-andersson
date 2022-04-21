c6ef258d4e5cb483ef80edd678b62f66eb523544
Anton Ekström <anton.e.ekstrom@gmail.com>
2022-04-21 11:27:00 +0200

    Adds cool timeline view

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,31 +1,54 @@
-import EventCard, { EventCardProps } from "../src/EventCard";
+import { Event } from "../lib/types";
+import { Timeline } from "../src/components/Timeline";
 
-const theKingsManPU: EventCardProps = {
-  thumbnailUrl: "./img/kingsman.jpg",
-  title: "PU-Movie: The King's Man",
-  price: "40 kr",
-  description: "A dope spy-movie about agents in the UK.",
-  link: "https://www.facebook.com/events/340855818101825/?ref=newsfeed",
-  committee: {
-    name: "PU",
-    imageUrl: "./img/pu.jpg",
-  },
-};
-
+/**
+ * This function runs in the backend and can be used to fetch the events from the database in the future.
+ * For now we are using mocked data for testing.
+ */
 export function getServerSideProps() {
   return {
     props: {
-      events: [theKingsManPU],
+      events: [
+        {
+          id: "2",
+          title: "PU-Movie: The Kingen 2",
+          link: "https://www.facebook.com/events/340855818101825/?ref=newsfeed",
+          date: new Date("2020-06-02").toJSON(),
+          host: "PU 2",
+        },
+        {
+          id: "1",
+          title: "Plupp movie",
+          link: "https://www.facebook.com/events/340855818101825/?ref=newsfeed",
+          date: new Date("2020-06-01").toJSON(),
+          host: "Plupp Inc",
+        },
+        {
+          id: "1",
+          title: "PU-Movie: The Kingen",
+          link: "https://www.facebook.com/events/340855818101825/?ref=newsfeed",
+          date: new Date("2020-06-01").toJSON(),
+          host: "PU",
+        },
+        {
+          id: "1",
+          title: "jämK-Movie: The Queenen",
+          link: "https://www.facebook.com/events/340855818101825/?ref=newsfeed",
+          date: new Date("2020-07-12").toJSON(),
+          host: "jämK",
+        },
+      ],
     },
   };
 }
 
-export default function Index({ events }: { events: EventCardProps[] }) {
+export default function Index({ events }: { events: Event[] }) {
   return (
-    <div className="p-8">
-      {events.map((event) => (
-        <EventCard {...event} />
-      ))}
+    <div className="grid place-items-center">
+      <div className="p-8 max-w-screen-xl w-full">
+        <h1 className="text-5xl -skew-x-12 mt-6 mb-16 ">Vad händer på campus?</h1>
+        <Timeline events={events} />
+      </div>
     </div>
   );
 }
--- client/src/components/Timeline.tsx
+++ client/src/components/Timeline.tsx
@@ -5,20 +5,100 @@ export type TimelineProps = {
 };
 
 export function Timeline({ events }: TimelineProps) {
+  const groupedDates = groupEvents(
+    events.sort((a, b) => (new Date(a.date) as any) - (new Date(b.date) as any))
+  );
+
+  console.log({ groupedDates });
+
   return (
-    <div>
-      {events.map((event) => (
-        <TimelineEvent key={event.id} {...event} />
+    <div className="flex flex-col gap-12">
+      {groupedDates.map((group) => (
+        <div className="">
+          <h1 className="text-3xl font-semibold mb-4">
+            {getDateFromEvent(group[0])}
+          </h1>
+          <div className="flex flex-col gap-2">
+            {group.map((event) => (
+              <TimelineEvent {...event} />
+            ))}
+          </div>
+        </div>
       ))}
     </div>
   );
 }
 
-function TimelineEvent({ title, link }: Event) {
+function TimelineEvent({ title, link, host }: Event) {
   return (
-    <div>
-      <h1>{title}</h1>
-      <a href={link.toString()}>link</a>
-    </div>
+    <a href={link.toString()} className="" target="_blank">
+      <div className="bg-white py-3 px-6 rounded-lg">
+        <div className="flex place-items-center gap-2 mb-3">
+          <h1 className="text-2xl text-sky-700 leading-none">{title}</h1>
+          <div className="text-gray-700">
+            <ExternalLinkIcon />
+          </div>
+        </div>
+        <p className="font-semibold text-lg leading-none">{host}</p>
+      </div>
+    </a>
+  );
+}
+
+function groupEvents(dates: Event[]) {
+  const groupedDates = [[dates[0]]];
+
+  for (let i = 0; i < dates.length; i++) {
+    const current = dates[i];
+    const next = dates[i + 1];
+
+    if (!next) {
+      break;
+    }
+
+    if (
+      new Date(current.date).getUTCDate() === new Date(next.date).getUTCDate()
+    ) {
+      groupedDates[groupedDates.length - 1].push(next);
+    } else {
+      groupedDates.push([next]);
+    }
+  }
+
+  return groupedDates;
+}
+
+function getDateFromEvent(event: Event): string {
+  const date = new Date(event.date);
+  const month = date.toLocaleString("sv", { month: "long" });
+  const day = date.getDate();
+  return `${day} ${capitalize(month)}`;
+}
+
+/**
+ * Capitalize the first letter of a string.
+ *
+ * @returns the string
+ */
+function capitalize(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function ExternalLinkIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+      />
+    </svg>
   );
 }
