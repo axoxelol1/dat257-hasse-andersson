0b1b236b9c4147da42352e687f5629c2919a25f0
Anton Ekström <anton.e.ekstrom@gmail.com>
2022-04-25 09:59:23 +0200

    Changes from next Image again because img

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -49,7 +49,7 @@ export default function Index({ events }: { events: Event[] }) {
     <div className="grid place-items-center">
       <div className="p-8 max-w-screen-xl w-full">
         <div className="flex flex-row place-items-center">
-          <Image src="/img/VHPC (1).png" alt="Website logo" className="w-32 h-32" />
+          <img src="/img/VHPC (1).png" alt="Website logo" className="w-32 h-32" />
           <h1 className="text-5xl -skew-x-12">Vad händer på campus?</h1>
         </div>
         <div className="flex flex-row w-full gap-4 mt-6">
