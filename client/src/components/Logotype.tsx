bb9c36c0ec391c623dd7e827c90a241c0fc1696a
Anton Ekström <anton.e.ekstrom@gmail.com>
2022-04-28 14:50:50 +0200

    Changes to nextjs Image on the logo, and hides text on smaller devices

--- client/src/components/Logotype.tsx
+++ client/src/components/Logotype.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 export function Logotype() {
   return (
-    <div className="flex flex-row place-items-center">
-      <img src="/img/VHPC_logo.png" alt="Website logo" className="w-48 h-48" />
-      <h1 className="text-3xl font-bold">Vad händer på campus?</h1> 
+    <div className="flex flex-row place-items-center relative">
+      <Image
+        src="/img/VHPC_logo.png"
+        alt="Website logo"
+        width={1079 / 8}
+        height={315 / 8}
+      />
+      <h1 className="text-3xl font-bold hidden md:block">Vad händer på campus?</h1>
     </div>
   );
 }
