9ccc060cfa1424a12b405118125a49102b53200b
LilleLink <simon_0_0_@live.se>
2022-04-28 15:01:34 +0200

    Adds types for gasque api calls

--- client/lib/types.ts
+++ client/lib/types.ts
@@ -12,3 +12,28 @@ export interface Event {
     eventImageUrl? : string;
     location? : string;
 }
+
+export interface GasqueEvent {
+    id: string;
+    date: string;
+    club_ticket_price_chalmers: number;
+    club_ticket_price_others: number;
+    dinner_ticket_price_alcohol: number;
+    dinner_ticket_price_no_alcohol: number;
+    is_dinner: number;
+    created_at: string;
+    updated_at: string;
+    facebook_event_url: string;
+    club_start_time: string;
+    club_end_time: string;
+    dinner_start_time: string;
+    is_club: number;
+    priority: unknown;
+    host_names: string[];
+    posters: GasqueEventPosters;
+}
+
+export interface GasqueEventPosters {
+    fullscreen_url: string;
+    portrait_url: string;
+}
