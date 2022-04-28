2cd82d6c2d8514ecf1b1e11ee148b90ff3b626c2
LilleLink <simon_0_0_@live.se>
2022-04-28 15:00:59 +0200

    Changes to custom server in TS format

---
+++ client/server.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import next from 'next';
+import { NextServer, RequestHandler } from 'next/dist/server/next';
+
+const port = 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app : NextServer = next({ dev });
+const handle : RequestHandler = app.getRequestHandler();
+
+app.prepare().then(() => {
+    const server = express();
+
+    server.all('*', (req : Request, res : Response ) => handle(req, res));
+
+    server.listen(port, () => {
+        console.log("Server listening on port " + port);
+    });
+
+});
