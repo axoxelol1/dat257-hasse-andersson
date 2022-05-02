8953820a10470886ac5ba37c0f0092d94415064f
Axel Larsson <axoxelol_01@hotmail.com>
2022-05-02 16:03:13 +0200

    Adds barebones calendar view

--- client/pages/calendar.tsx
+++ client/pages/calendar.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { DataService } from "../lib/data.service";
 import { DatabaseService } from "../lib/db.service";
 import { Host, Event } from "../lib/types"
+import { Month } from "../src/components/calendar/Month";
 
 /**
  * This function runs in the backend and is used to fetch the events from the data sources.
@@ -15,8 +17,36 @@ import { Host, Event } from "../lib/types"
 }
 
 export default function Calendar({events, hosts }: { events: Event[], hosts: Host[] }) {
+  const [date, setDate] = useState(new Date());
+
+  const nextMonth = () => {
+    setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1));
+  };
+
+  const prevMonth = () => {
+    setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1));
+  };
+  
   return (
-    <div>Hello world</div>
+    <>
+      <div>
+        <h1 className="text-6xl font-semibold">
+          Calendar
+        </h1>
+      </div>
+      <div className="flex justify-center">
+        <div className="flex flex-col">
+          <div className="flex justify-center flex-row gap-x-3">
+            <button onClick={prevMonth}>{"<"}</button>
+            <div className="w-24 text-center">
+             {date.toLocaleString("en-us", { month: "short", year: "numeric" })}
+            </div>
+            <button onClick={nextMonth}>{">"}</button>
+          </div>
+          <Month events={events} month={date.getMonth()} />
+        </div>
+      </div>
+    </>
   )
 }
 
