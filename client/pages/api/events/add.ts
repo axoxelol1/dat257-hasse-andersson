cc95d9ec6e3d3da18314aa788c1f4111dbb39dfe
LilleLink <simon_0_0_@live.se>
2022-05-03 22:50:06 +0200

    Adds server-side validation of required event fields

--- client/pages/api/events/add.ts
+++ client/pages/api/events/add.ts
@@ -7,10 +7,10 @@ import { DatabaseService } from "../../../lib/db.service";
 import { Event } from "../../../lib/types";
 
 export default async function add(req: NextApiRequest, res: NextApiResponse) {
-  const event: Event = JSON.parse(req.body);
+  const event: Event = req.body;
 
   if (!inputIsValid(event)) {
-    res.status(400).json({ error: "Missing required fields. Required fields are: title, host, date and time" });
+    res.status(400).send({ error: "Missing required fields. Required fields are: title, host, date and time" });
     return;
   }
 
@@ -23,5 +23,7 @@ export default async function add(req: NextApiRequest, res: NextApiResponse) {
 }
 
 function inputIsValid(event: Event): boolean {
-  return event.title == "" || event.date == "" || event.host == "";
+  // Wonky notation, I know, just easier for me to read
+  // Just checks that the event has all the required fields
+  return !(event.title == "" || event.date == "" || event.host == "");
 }
