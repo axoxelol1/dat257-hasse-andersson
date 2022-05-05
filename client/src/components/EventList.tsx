99f923669cec5fb698b8ea53adc28e9ac3561668
LilleLink <simon_0_0_@live.se>
2022-05-05 15:11:49 +0200

    Adds button to filter out past events

--- client/src/components/EventList.tsx
+++ client/src/components/EventList.tsx
@@ -5,6 +5,7 @@
 
 import { Event } from "../../lib/types";
 import { TimelineEvent } from "./Timeline";
+import { useState } from "react";
 
 export interface EventListProps {
   events: Event[];
@@ -12,15 +13,27 @@ export interface EventListProps {
 
 export default function EventList(props : EventListProps) {
 
+  const [showPastEvents, setShowPastEvents] = useState(false);
+
   return (
     <>
       <div className="flex flex-col w-full">
-        <span className="text-xl font-bold">Events</span>
-        <div className="flex flex-col py-2 gap-y-2 border-t-2 border-black">
-          {props.events.sort((a,b) => (Date.parse(a.date) < Date.parse(b.date)) ? 1 : -1).map((event,id) => (
+        <span className="text-xl w-full border-b-2 border-black font-bold">Events</span>
+        
+        <div>
+          <input type="checkbox" className="text-sm font-bold" name="pastevents" onClick={() => setShowPastEvents(!showPastEvents)}/>
+          <label> Show past events</label>
+        </div>
+
+        <div className="flex flex-col py-2 gap-y-2 ">
+          {props.events
+            .filter((e) => !(Date.parse(e.date) < Date.now()) || showPastEvents)
+            .sort((a,b) => (Date.parse(a.date) < Date.parse(b.date)) ? 1 : -1)
+            .map((event,id) => (
             <TimelineEvent key={id} {...event} />
           ))}
         </div>
+        
       </div>
     </>
   )
