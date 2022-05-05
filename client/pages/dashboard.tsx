d3904eedd9b47f3d0fd61dbc3b5d7f8f78f0a155
LilleLink <simon_0_0_@live.se>
2022-05-05 15:32:07 +0200

    Changes logo and title to Logotype component

--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -2,12 +2,12 @@
  * Dashboard page used to add events manually to the database.
  */
 
-import Image from "next/image";
 import EventForm from "../src/components/EventForm";
 import EventList from "../src/components/EventList";
 import { Event } from "../lib/types";
 import { DatabaseService } from "../lib/db.service";
 import { useState } from "react";
+import { Logotype } from "../src/components/Logotype";
 
 export async function getServerSideProps() {
   return {
@@ -31,15 +31,7 @@ export default function Dashboard ({ events } : {events : Event[]}) {
   return (
     <div className="grid place-items-center">
       <div className="p-8 max-w-screen-xl w-full">
-        <div className="flex flex-row place-items-center">
-          <Image
-            src="/img/VHPC (1).png"
-            alt="Website logo"
-            width={"96"}
-            height={"96"}
-          />
-          <h1 className="text-5xl -skew-x-12">VHPC - Dashboard</h1>
-        </div>
+        <Logotype/>
         <div className="flex flex-col md:flex-row w-full gap-4 mt-6">
           <EventForm updateEventList={updateEventList}/>
           <EventList events={eventList}/>
