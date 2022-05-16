acf787ed64b464cbf14523e628af7dfe2e7efcb4
Jacob <jacob.bengtsson@rocketmail.com>
2022-05-16 21:30:15 +0200

    Actually does it

--- client/pages/dashboard/_middleware.ts
+++ client/pages/dashboard/_middleware.ts
@@ -1,20 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server'
-import jose from "jose"
+import * as jose from "jose"
 
 /*
   Intercepts requests to pages in its directory and handles the requests accordingly
 */
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const token = req.cookies?.JWT
 
   try {
     //Using jose instad of the normal JWT-library since JWT can't run in Edge Runtime where this middleware happens
-    jose.jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET))
+    await jose.jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET))
 
     //If token is invalid, an error is thrown sending us to the catch-statement
     return NextResponse.next()
-  } catch {
+  } catch (err) {
+    console.log(err)
     return NextResponse.json("User not logged in")
   }
 }
