2c4a890ca7c87c57eed83eeaefe77e05edc9c0b7
Anton Ekström <anton.e.ekstrom@gmail.com>
2022-05-18 17:31:11 +0200

    Fixes CalendarExport on large screens

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -61,6 +61,7 @@ export default function Index({
                       )
                 }
                 hosts={hosts}
+                selectedHosts={selectedHosts}
               />
             </div>
           </div>
