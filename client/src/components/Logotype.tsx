5b8746da41bf2f7db325f99d6046c78d9151a31b
Cabbethebaos <casper.lindh@gmail.com>
2022-05-18 14:25:05 +0200

    Added link

--- client/src/components/Logotype.tsx
+++ client/src/components/Logotype.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useRef } from "react";
 
 export function Logotype() {
   return (
     <div className="flex flex-row place-items-center relative">
-      <Image
-        src="/img/VHPC_logo.png"
-        alt="Website logo"
-        width={1079 / 12}
-        height={315 / 12}
-      />
+      <Link href={"http://localhost:3000/"}>
+        <a>
+          <Image
+            src="/img/VHPC_logo.png"
+            alt="Website logo"
+            width={1079 / 12}
+            height={315 / 12}
+          />
+        </a>
+      </Link>
       <h1 className="text-3xl font-bold hidden md:block">Vad händer på campus?</h1>
     </div>
   );
