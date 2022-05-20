7c54c45a008acbfb1edd35007cae99544003d02a
Anton Ekström <anton.e.ekstrom@gmail.com>
2022-05-20 10:18:41 +0200

    Changes filters to be closed by default

--- client/src/components/Filters.tsx
+++ client/src/components/Filters.tsx
@@ -21,7 +21,7 @@ export default function Filters(props: FiltersProps) {
 }
 
 function HostFilter({ onChange, hosts }: FiltersProps) {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const [selectedHosts, setSelectedHosts] = useState<Host[]>([]);
   const [query, setQuery] = useState("");
 
